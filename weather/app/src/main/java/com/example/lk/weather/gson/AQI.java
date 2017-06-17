2b7e8fcb39ddae2a6ec52d68bf5570c0d06676e5
Fallings <1363597974@qq.com>
2017-06-17 17:52:56 +0800

    weather

---
+++ weather/app/src/main/java/com/example/lk/weather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.lk.weather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi = null;
+
+        public String pm25 = null;
+
+        public String qlty =null;
+
+    }
+
+}
